29ec6ba596a0a7dfdc6256a6132951616ef9bb8e
alymych <alymych@atlassian.com>
2023-09-29 20:58:59 +0300

    added env config

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const start = async () => {
+  const PORT = process.env.PORT || 5000;
+
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000);
+
+  await app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server is listening ${PORT} port...`);
+  });
 };
 
 start();
